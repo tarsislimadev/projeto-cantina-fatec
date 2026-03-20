9d3188a816e466527c78fe9497f351ad3e943a8d
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-20 21:49:05 +0000

    Add main project classes

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
+class Produto:
+  def __init__(self, nome, preco_compra, preco_venda, data_compra, data_vencimento, quantidade):
+    self.nome = nome
+    self.preco_compra = preco_compra
+    self.preco_venda = preco_venda
+    self.data_compra = data_compra
+    self.data_vencimento = data_vencimento
+    self.quantidade = quantidade
+
+  def atualizar_a_quantidade(self, nova_qtd):
+    self.quantidade = nova_qtd
+
+class Pagamento:
+  def __init__(self, nome, categoria, curso, valor, data_hora):
+    self.nome = nome
+    self.categoria = categoria
+    self.curso = curso
+    self.valor = valor
+    self.data_hora = data_hora
+
+class Consumo:
+  def __init__(self, consumidor, produto, pagamento):
+    self.consumidor = consumidor
+    self.produto = produto
+    self.pagamento = pagamento
+
 def menu():
   while True:
     print('-- menu --')
